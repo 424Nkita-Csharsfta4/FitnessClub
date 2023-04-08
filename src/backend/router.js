dbf58454d50b423b89d294b46eb7f2dd830bb1bc
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-04-08 23:13:25 +0300

    Delete router.js

--- src/backend/router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var router = (0, express_1.Router)();
-router.get('/foo', function (req, res) {
-    res.send('Hello, world!');
-});
-exports.default = router;
