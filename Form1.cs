dbeac1fdb036ac0ed6090de085857cac55b8dac6
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-05-08 17:35:01 +0300

    Update Form1.cs
    Добавил комментарии к коду

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,18 +27,21 @@ namespace WindowsFormsApp1
         {
             var namee = textBox1.Text;
             var pas = textBox2.Text;
+            // Создание адаптера данных и таблицы для хранения результата запроса
             SqlDataAdapter sqladapter = new SqlDataAdapter();
             DataTable table = new DataTable();
+            // Формирование запроса на выборку пользователей с указанным именем и паролем
             string Query = $"SELECT iid, namee, podpis, password FROM users WHERE namee = '{namee}' AND password = '{pas}'";
             SqlCommand command = new SqlCommand(Query, BD.GetConnection());
 
+// Выполнение запроса и сохранение результатов в таблице
             sqladapter.SelectCommand = command;
             sqladapter.Fill(table);
 
 
             if (table.Rows.Count == 1)
             {
-                
+                  // Если в таблице одна строка, то пользователь существует и вход выполнен успешно
                 MessageBox.Show($"Вы зашли под именем: {namee}", "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 ClubMain Club = new ClubMain();
                 this.Hide();
@@ -48,6 +51,7 @@ namespace WindowsFormsApp1
             }
             else
             {
+            // Если в таблице нет строк, то пользователь не найден
                 MessageBox.Show($"Такого имени в БД  нет", "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
