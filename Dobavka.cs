78c486200207fe171653e7b17607df5214410516
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-05-08 17:23:41 +0300

    Update Dobavka.cs
    Добавил комментарии к коду

--- Dobavka.cs
+++ Dobavka.cs
@@ -14,35 +14,46 @@ namespace WindowsFormsApp1
 {
     public partial class Dobavka : Form
     {
+    // Объект класса работы с базой данных
         BD BD = new BD();
         public Dobavka()
         {
             InitializeComponent();
         }
-
+/// <summary>
+/// Обработчик нажатия на кнопку добавления тренировки
+/// </summary>
         private void button1_Click(object sender, EventArgs e)
         {
+        // Открытие соединения с БД
             BD.openConnection();
-
+// Название тренировки
             var NameTrain = textBox1.Text;
+            // ФИО тренера
             var Trainer = textBox2.Text;
+             // Время проведения тренировки
             var TimeTrain = dateTimePicker1.Value.ToString();
+             // Стоимость тренировки
             int price;
 
+// Если введенная стоимость является числом
             if(int.TryParse(textBox4.Text, out price))
             {
+            // Формирование SQL-запроса на добавление тренировки в БД
                 var dobqur = $"INSERT INTO trains (NameTrain,Trainer,TimeTrain,price) VALUES ('{NameTrain}', '{Trainer}', '{TimeTrain}', '{price}')";
                 var cmd = new SqlCommand(dobqur, BD.GetConnection());
+                // Выполнение SQL-запроса на добавление тренировки в БД
                 cmd.ExecuteNonQuery();
-
+// Вывод сообщения об успешном добавлении тренировки
                 MessageBox.Show("Вы записались на тренировку", "Успехх", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-
+// Если введенная стоимость не является числ
             else
             {
+             // Вывод сообщения об ошибке добавления тренировки
                 MessageBox.Show("Вы не оплатили тренировку ", "Не Успехх", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-
+// Закрытие соединения с БД
             BD.closeConnection();
         }
 
